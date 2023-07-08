61007fbd01bbb1829f0213c295805f41088fa555
MichalStanaszek <michal.stanaszek@gmail.com>
2023-07-08 21:20:11 +0200

    add libraries + api-key
    - axios
    - notiflix
    - slim-select

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_HlIOy0zcfezhfHUXznjThXXmpIUKAJ90Tj3QthYf4iphxUH2lPkRN3O4iSlFd4My';
