b54e9cd72468831065caad45a83320b06bedc621
MichalStanaszek <michal.stanaszek@gmail.com>
2023-07-10 22:43:10 +0200

    feat: rendering single data from select dropdown

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,47 +1,26 @@
 import axios from 'axios';
 
-const breedSelect = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-const catInfoContainer = document.querySelector('.cat-info');
-const url = 'https://api.thecatapi.com/v1/breeds';
-const apiKey =
-  'live_HlIOy0zcfezhfHUXznjThXXmpIUKAJ90Tj3QthYf4iphxUH2lPkRN3O4iSlFd4My';
+//config function
+export function init(api_key) {
+  axiosCatApi = require('axios').default;
+  axiosCatApi.defaults.headers.common['x-api-key'] = api_key;
+}
 
-axios.defaults.headers.common['x-api-key'] = apiKey;
-
-// fetchBreeds() wysyła zapytanie na serwer i pobiera tablicę obiektów
+//capturing array of all objects from the server
 export function fetchBreeds() {
-  axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(response => {
-      const breeds = response.data;
-      fillBreedSelect(breeds);
-    })
+  return axiosCatApi
+    .get(`https://api.thecatapi.com/v1/breeds`)
+    .then(response => response.data)
     .catch(error => {
       console.error(error);
     });
 }
-
-// Funkcja wypełniająca select opcjami ras kotów
-function fillBreedSelect(breeds) {
-  breeds.forEach(breed => {
-    const option = document.createElement('option');
-    option.value = breed.id;
-    option.textContent = breed.name;
-    breedSelect.appendChild(option);
-  });
+//capturing 1 object from the server
+export function fetchCatByBreed(breedId) {
+  return axiosCatApi
+    .get(`https://api.thecatapi.com/v1/images/${breedId}`)
+    .then(response => response.data)
+    .catch(error => {
+      console.error(error);
+    });
 }
-const breeds = fetchBreeds();
-
-// //fetchCatByBreed() pobiera konkretne ID kota z serwera
-// export function fetchCatByBreed(breedId) {
-//   axios
-//     .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-//     .then(response => {
-//       const catInfo = response.data[0];
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
-// }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import axios from 'axios';
+import * as catApi from './cat-api';
 import SlimSelect from 'slim-select';
+import '../node_modules/slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
-import { fetchBreeds } from './cat-api';
-import { fetchCatByBreed } from './cat-api';
 
 const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
-const catInfoContainer = document.querySelector('.cat-info');
 
-// Dodanie nasłuchiwania na zdarzenie zmiany dla select
-breedSelect.addEventListener('change', handleSelectChange);
+const apiKey =
+  'live_7RIdaPQIKQeuQ4h5OHhIknuCKcTJHhxKZ6jGEa1GCIsFM4TeRDRzpC67rBRSSXOw';
 
-function handleSelectChange(event) {
-  const breedId = event.target.value;
+breedSelect.addEventListener('change', handleChange);
+
+function addList(items) {
+  const markup = items
+    .map(item => {
+      return `<option value=${item.reference_image_id}>${item.name} </option>`;
+    })
+    .join('');
+  breedSelect.innerHTML = markup;
 }
+
+//creating post on website with cat name, image and descripion
+function addPost(item) {
+  const markup = `
+  <img src="${item.url}" alt="">
+  <div>
+    <h1>${item.breeds[0].name}</h1>
+    <p>${item.breeds[0].description}</p>
+    <p><b>Temperament: </b>${item.breeds[0].temperament}</p>
+  </div>
+  `;
+  catInfo.innerHTML = markup;
+}
+
+function handleChange(event) {
+  loader.classList.remove('hidden');
+  catInfo.classList.add('hidden');
+  catApi
+    .fetchCatByBreed(event.currentTarget.value)
+    .then(function (response) {
+      addPost(response);
+      loader.classList.add('hidden');
+      catInfo.classList.remove('hidden');
+      error.classList.add('hidden');
+    })
+    .catch(function (error) {
+      Notiflix.Notify.failure(
+        'Upps! Coś poszło nie tak. Odśwież stronę jeszcze raz.'
+      );
+      error.classList.remove('hidden');
+      loader.classList.add('hidden');
+      console.log(error);
+    });
+}
+
+catApi.init(apiKey);
+catApi
+  .fetchBreeds()
+  .then(function (response) {
+    addList(response);
+    loader.classList.add('hidden');
+    breedSelect.classList.remove('hidden');
+    error.classList.add('hidden');
+    const select = new SlimSelect({
+      select: breedSelect,
+    });
+  })
+  .catch(function (error) {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+    error.classList.remove('hidden');
+    loader.classList.add('hidden');
+    console.log(error);
+  });
